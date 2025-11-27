697ce36bc450569a8cf86baa0c38f187a82184ff
emoniates <your_email@example.com>
2025-11-28 02:33:04 +0300

    Add a checked in Property

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-//Write your code here
+// Create the attendee object
+const attendee = {
+    attendeeId: "T001",
+    name: "Alice Smith",
+    event: "JavaScript Conference",
+    ticketType: "VIP",
+    ticketPrice: 150.00
+};
 
+// Function to log the attendee's name
+function logAttendeeName(attendee) {
+    console.log(attendee.name);
+}
 
+// Function to log the ticket price
+function logTicketPrice(attendee) {
+    console.log(attendee.ticketPrice);
+}
 
-//Needed for the tests to work. Don't modify
+// Function to update the ticket type
+function updateTicketType(attendee, newTicketType) {
+    attendee.ticketType = newTicketType;
+}
+
+// Function to update the ticket price
+function updateTicketPrice(attendee, newTicketPrice) {
+    attendee.ticketPrice = newTicketPrice;
+}
+
+// Function to remove the 'event' property
+function removeEventProperty(attendee) {
+    delete attendee.event;
+}
+
+// Function to add 'checkedIn' property
+function addCheckedInProperty(attendee) {
+    attendee.checkedIn = true;
+}
+
+// Export everything for Jest tests
 module.exports = {
-  ...(typeof attendee !== 'undefined' && { attendee }),
-  ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
-  ...(typeof logTicketPrice !== 'undefined' && { logTicketPrice }),
-  ...(typeof updateTicketType !== 'undefined' && { updateTicketType }),
-  ...(typeof updateTicketPrice !== 'undefined' && { updateTicketPrice }),
-  ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
-  ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
+    attendee,
+    logAttendeeName,
+    logTicketPrice,
+    updateTicketType,
+    updateTicketPrice,
+    removeEventProperty,
+    addCheckedInProperty
 };
